6d693357866d7e0ef78c2e3d35f81c7bcbe55ed5
Lin HE <104896569+Lin0He@users.noreply.github.com>
2026-03-02 01:11:20 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router basename="/Travel_webpage_AI">
+    <Router basename="/Travel_webpage">
       <Routes>
         <Route path="/*" element={<App />}/> 
         //<Route path="/response" element={<Response />}/>
